deea06c95988a5d09d595427593f6b737fec9060
Markkelvin14 <mjkelvin38@gmail.com>
2023-09-25 16:25:49 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID"""
+'''
+    this module contains the function get_info
+'''
 import requests as req
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_id = req.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = req.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(user_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+def get_infomation(employee_id):
+    '''returns information for a given employee Id'''
+    user_id = argv[1]
+    user = req.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(user_id)).json()
+    todo = req.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(user_id)).json()
+    name = user.get('name')
+    done_tasks = []
+    done_count = 0
+    total_count = 0
+    for task in todo:
+        total_count += 1
+        if task.get('completed'):
+            done_tasks.append(task.get('title'))
+            done_count += 1
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, done_count, total_count))
+    for task in done_tasks:
+        print('\t {}'.format(task))
+
+
+if __name__ == "__main__":
+    get_infomation(int(argv[1]))
